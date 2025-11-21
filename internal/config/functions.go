9093a6d4d4aa342a682c9973d180a293bba3fbba
Adfmu1 <adfmu3@gmail.com>
2025-11-21 17:59:57 +0100

    create SetUser method for Config struct

--- internal/config/functions.go
+++ internal/config/functions.go
@@ -6,22 +6,44 @@ import (
 	"os"
 )
 
+// ============ CONSTANTS ============
+const configFileName = "/gatorconfig.json"
+
+// ============ HELPER FUNCTIONS ============
 func CheckPrintErr(err error) {
 	if err != nil {
 		fmt.Println("an error has occured:", err)
 	}
 }
 
+// ============ MAIN FUNCTIONS ============
+// READ THE CONFIG FILE
 func Read() Config {
-	conf := Config{}
 	// get config file path
 	confPath, err := os.UserHomeDir()
 	CheckPrintErr(err)
-	confFilePath := confPath + "/gatorconfig.json"
+	confFilePath := confPath + configFileName
 	// get config file contents
 	configData, err := os.ReadFile(confFilePath)
 	CheckPrintErr(err)
+	// unmarshal the data
+	conf := Config{}
 	err = json.Unmarshal(configData, &conf)
 	CheckPrintErr(err)
 	return conf
+}
+
+// WRITE NEW USER TO CONFIG FILE
+func (conf Config) SetUser(userName string) {
+	// set new username in struct
+	conf.CurrentUser = userName
+	// get config file path
+	confPath, err := os.UserHomeDir()
+	CheckPrintErr(err)
+	confFilePath := confPath + configFileName
+	// marshal the data
+	data, err := json.Marshal(conf)
+	CheckPrintErr(err)
+	err = os.WriteFile(confFilePath, data, 0666)
+	CheckPrintErr(err)
 }
--- internal/config/structs.go
+++ internal/config/structs.go
@@ -2,4 +2,5 @@ package config
 
 type Config struct {
 	DatabaseURL string `json:"db_url"`
+	CurrentUser string `json:"current_user_name"`
 }
